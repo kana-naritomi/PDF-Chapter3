85ab0eff767ef1bcc345400790da037b75677791
kana-naritomi <kanaritomi83@gmail.com>
2022-07-06 22:34:04 +0900

    学習記録

---
+++ PDF-Chapter4/27-変数への代入とその確認方法/script.js
@@ -0,0 +1,31 @@
+// 1.
+var variableNumber = 1234;
+console.log(variableNumber);
+
+// 2.
+var variableText = "Text";
+console.log(variableText);
+
+// 3.
+var variableBoolean = true;
+console.log(variableBoolean);
+
+// 4.
+var variableNumber = 10 + 20;
+console.log(variableNumber);
+
+// 5.
+var variableNumber = 20 - 10;
+console.log(variableNumber);
+
+// 6.
+var variableNumber = 50 * 2;
+console.log(variableNumber);
+
+// 7.
+var variableNumber = 20 / 2;
+console.log(variableNumber);
+
+// 8.
+var variableText = "12" + "34";
+console.log(variableText);
