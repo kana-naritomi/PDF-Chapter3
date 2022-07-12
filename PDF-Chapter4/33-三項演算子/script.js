3aa6425042d92190d04d7dac3d995f2a1e9bdd8c
kana-naritomi <kanaritomi83@gmail.com>
2022-07-12 20:06:25 +0900

    学習記録

---
+++ PDF-Chapter4/33-三項演算子/script.js
@@ -0,0 +1,20 @@
+// 1.
+var boolean = true;
+var resultText = "";
+var resultText = boolean ? "Aの処理" : "Bの処理";
+
+// 2.
+var num = 7;
+var resultNum = num > 5 ? true : false;
+
+// 3.
+// var num = 7;
+var resultNum = num > 5 ? true : false;
+
+// 4.
+var num = 15; 
+var resultNum = num >= 10 ? true : false;
+
+// 5.
+var num = 1;
+var resultNum = num >= 1 ? true : false;
